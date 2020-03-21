34ee3b6e5c5da7083e963db33e3c55d97182b38a
Arjan <midborghmorgenstern@gmail.com>
2020-03-21 14:07:15 +0100

    small bugfixes

--- StatusBot.py
+++ StatusBot.py
@@ -8,7 +8,7 @@ localIp = '127.0.0.1'
 queryPortString = 25565
 rconPortString = 25575
 rconPassword = ""
-discordSecret = ""
+discordToken = ""
 discordChannelName = ""
 
 # Read all settings and write to variables
@@ -26,8 +26,10 @@ for setting in settings:
         queryPortString = settingValue
     elif settingId == "RCON-PORT":
         rconPortString = settingValue
-    elif settingId == "DISCORD-SECRET":
-        discordSecret = settingValue
+    elif settingId == "RCON-PASSWORD":
+        rconPassword = settingValue
+    elif settingId == "DISCORD-TOKEN":
+        discordToken = settingValue
     elif settingId == "DISCORD-CHANNEL-NAME":
         discordChannelName = settingValue
     else:
@@ -37,15 +39,15 @@ for setting in settings:
 queryPort = int(queryPortString)
 rconPort = int(rconPortString)
 
-
 # Check obligatory settings
 if serverUrl == "":
     print("ERROR. No serverurl given")
-if discordSecret == "":
-    print("ERROR. No discord secret given")
+if discordToken == "":
+    print("ERROR. No discord bot token given")
 if discordChannelName == "":
     print("ERROR. No discord channel given")
 
+#print(localIp + ':' + str(queryPort))
 localServer = MinecraftServer(localIp, queryPort)
 urlServer = MinecraftServer(serverUrl, queryPort)
 
@@ -60,12 +62,12 @@ class MyClient(discord.Client):
             urlLatency = -1
             localLatency = -1
             playerAmountOnline = -1
+            maxPlayerAmount = -1
             playerList = ""
             mapName = ""
 
             try:
                 urlStatus = urlServer.status()
-                playerAmountOnline = urlStatus.players.online
                 urlLatency = str(urlStatus.latency)
             except:
                 print("Error while contacting server over url")
@@ -73,44 +75,30 @@ class MyClient(discord.Client):
             try:
                 localStatus = localServer.status()
                 localLatency = str(localStatus.latency)
+                playerAmountOnline = localStatus.players.online
+                maxPlayerAmount = localStatus.players.max
+                for player in localStatus.players.sample:
+                    playerList += player.name + "\n"
             except:
                 print("Error while contacting server locally")
-
-            try:
-                query = localServer.query()
-                playerList = ", ".join(query.players.names)
-                mapName = query.map
-            except:
-                print("Error while executing query")
             
-
             response = "```"
-            response += 'Status report: \n'
-            response += "Domain is " + serverUrl + "\n"
-            
+            response += 'Status report for' + serverUrl + ': \n'
             if urlLatency != -1:
-                response += "The server replied over DNS in " + urlLatency + 'ms\n'
+                response += "The server replied over DNS in " + str(urlLatency) + 'ms\n'
             else:
                 response += "The server did not reply over DNS\n"
-
             if localLatency != -1:
-                response += "The server replied over the local network in " + localLatency + " ms\n"
+                response += "The server replied over the local network in " + str(localLatency) + " ms\n"
+                if playerAmountOnline > 0:
+                    response += "The server has " + str(playerAmountOnline) + "/" + str(maxPlayerAmount) + " players online.\n"
+                    response += "Online players: " + playerList + "\n"
+                else:
+                    response += "No players are currently playing. Please do something about that :)\n"
             else:
-                response += "The server did not reply over the local network"
-
-            if playerAmountOnline > 0:
-                response += "The server has " + str(playerAmountOnline) + " players online.\n"
-                response += "Online players: " + playerList + "\n"
-            else:
-                response += "No players are currently playing. Please do something about that :)\n"
-            
-            response += "Map: " + mapName + "\n"
-        
-
+                response += "The server did not reply over the local network\n"
             response += "```"
             await message.channel.send(response)
 
-
-
 client = MyClient()
-client.run(discordSecret)
+client.run(discordToken)
